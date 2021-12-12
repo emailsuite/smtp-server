21bb25fe491873cc5f531154b6b17d793c697371
Andrey Reinwald <andreinwald@gmail.com>
2021-12-12 22:16:05 +0200

    fixed test

--- test.php
+++ test.php
@@ -4,19 +4,37 @@ include __DIR__ . "/SocketManager.php";
 
 $sm = new SocketManager();
 
-$toEmail = 'recipient@example.com';
-//$domain = explode('@', $email)[1];
-//
-//$mxHosts = [];
-//getmxrr($domain, $mxHosts);
-//$host = $mxHosts[0];
-$host = 'mail.smtpbucket.com';
-$port = 8025;
+$toEmail = 'test@gmail.com';
+$domain = explode('@', $toEmail)[1];
+$mxHosts = [];
+getmxrr($domain, $mxHosts);
+$host = $mxHosts[0];
+$port = 25;
 $fromEmail = 'sender@example.com';
 
 $socketId = $sm->openSocket($host, $port);
-echo $sm->sendMessage($socketId, "helo $host");
-echo $sm->sendMessage($socketId, "mail from:<$fromEmail>");
-echo $sm->sendMessage($socketId, "rcpt to:<$toEmail>");
-echo $sm->sendMessage($socketId, "data");
-echo $sm->sendMessage($socketId, "some test email \n.");
+list($code, $message) = $sm->sendMessage($socketId, "helo $host");
+echo $message;
+if ($code > 300) {
+    die;
+}
+list($code, $message) =  $sm->sendMessage($socketId, "mail from:<$fromEmail>");
+echo $message;
+if ($code > 300) {
+    die;
+}
+list($code, $message) =  $sm->sendMessage($socketId, "rcpt to:<$toEmail>");
+echo $message;
+if ($code > 300) {
+    die;
+}
+list($code, $message) =  $sm->sendMessage($socketId, "data");
+echo $message;
+if ($code > 300) {
+    die;
+}
+list($code, $message) =  $sm->sendMessage($socketId, "subject: My Telnet Test Email \n some test email \r\n.");
+echo $message;
+if ($code > 300) {
+    die;
+}
