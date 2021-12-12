161b359f5de7895fefad32ae2de45407a41bee8b
Andrey Reinwald <andreinwald@gmail.com>
2021-12-12 22:15:52 +0200

    added response code

--- SocketManager.php
+++ SocketManager.php
@@ -2,6 +2,7 @@
 
 class SocketManager
 {
+    private const CRLF = "\r\n";
     private static $sockets = [];
     private static $socketOpened = [];
 
@@ -25,23 +26,27 @@ class SocketManager
         fclose($socketId);
     }
 
-    public function sendMessage($socketId, $message): string
+    public function sendMessage($socketId, $message): array
     {
-        $message = trim($message) . "\n";
+        $message = trim($message) . self::CRLF;
         $socket = self::$sockets[$socketId];
         fputs($socket, $message);
         return $this->readResponse($socket);
     }
 
-    private function readResponse($socket): string
+    private function readResponse($socket): array
     {
+        $code = 0;
         $response = '';
         while (($line = fgets($socket, 515)) !== false) {
             $response .= trim($line) . "\n";
+            if (!$code) {
+                $code = (int)substr($response, 0, 3);
+            }
             if ($line[3] === ' ') {
                 break;
             }
         }
-        return $response;
+        return [$code, $response];
     }
 }
